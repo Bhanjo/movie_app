a24cf2fe1c9b55eb472419f91bf31030e8b42e92
Bhanjo <hanjo1515@naver.com>
2020-04-07 11:23:45 +0900

    Updates

---
+++ precache-manifest.31ec96025cb0a4b3b1e06a445bb40966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4f60def513a8fb49ae84c36837175c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "daae00acdf078ee60479",
+    "url": "/movie_app/static/css/main.cc06df6e.chunk.css"
+  },
+  {
+    "revision": "840ed03df3d38d9b2441",
+    "url": "/movie_app/static/js/2.72ff5aa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.72ff5aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daae00acdf078ee60479",
+    "url": "/movie_app/static/js/main.91a7639c.chunk.js"
+  },
+  {
+    "revision": "ccd69d16ba9ce67ab39c",
+    "url": "/movie_app/static/js/runtime-main.730eb7a6.js"
+  }
+]);
